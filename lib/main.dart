10f399eee0287706ac02df1d05faeebcb5ad771a
cristhian ochoa <cristhho@gmail.com>
2019-10-21 23:33:38 -0500

    Cards, Columns and Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Weekly groceries', amount: 20.30, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Personal Expenses'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Card(
             color: Colors.blue,
@@ -31,6 +39,25 @@ class MyHomePage extends StatelessWidget {
           ),
           Card(
             child: Text('LIST'),
+          ),
+          Column(
+            children: transactions.map((trs) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text('\$' + trs.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(trs.title),
+                        Text(trs.date.toString())
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       )
