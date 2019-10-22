1d9cc09ff8a1e12e6320f283e9642a1ae0044cf1
cristhian ochoa <cristhho@gmail.com>
2019-10-21 23:46:43 -0500

    Text styling

--- lib/main.dart
+++ lib/main.dart
@@ -50,17 +50,22 @@ class MyHomePage extends StatelessWidget {
                           vertical: 10.0, horizontal: 15.0
                       ),
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black, width: 2)
+                          border: Border.all(color: Colors.purple, width: 2)
                       ),
                       padding: (
                         EdgeInsets.all(10.0)
                       ),
-                      child: Text('\$' + trs.amount.toString()),
+                      child: Text(
+                        '\$' + trs.amount.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                            fontSize: 20.0, color: Colors.purple),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(trs.title),
-                        Text(trs.date.toString())
+                        Text(trs.title, style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                        Text(trs.date.toString(), style: TextStyle(fontSize: 14.0, color: Colors.grey))
                       ],
                     )
                   ],
