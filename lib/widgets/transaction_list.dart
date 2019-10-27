205bb2fd87e9ec02af8ec67e661e4c98e1508b38
cristhian ochoa <cristhho@gmail.com>
2019-10-26 23:45:17 -0500

    styling a list title widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,59 +13,48 @@ class TransactionList extends StatelessWidget {
     return Container(
       height: 300,
       child: _transactions.isEmpty ?
-          Column(
-            children: <Widget>[
-              Text(
-                "No transactions added yet",
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset("assets/img/waiting.png", fit: BoxFit.cover,)
-              )
-            ],
-          )
-          : ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 15.0
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Theme.of(context).primaryColor, width: 2)
-                  ),
-                  padding: (
-                      EdgeInsets.all(10.0)
-                  ),
-                  child: Text(
-                    '\$${_transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: 20.0, color: Theme.of(context).primaryColor),
+        Column(
+          children: <Widget>[
+            Text(
+              "No transactions added yet",
+              style: Theme.of(context).textTheme.title,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset("assets/img/waiting.png", fit: BoxFit.cover,)
+            )
+          ],
+        )
+        : ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              elevation: 5,
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(6.0),
+                    child: FittedBox(
+                      child: Text('\$${_transactions[index].amount}')
+                    ),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(_transactions[index].title,
-                        style: Theme.of(context).textTheme.title
-                    ),
-                    Text(DateFormat('yyyy-MM-dd').format(_transactions[index].date),
-                        style: TextStyle(fontSize: 14.0, color: Colors.grey)
-                    )
-                  ],
-                )
-              ],
-            ),
-          );
-        },
-        itemCount: _transactions.length,
-      ),
+                title: Text(
+                  _transactions[index].title,
+                  style: Theme.of(context).textTheme.title,
+                ),
+                subtitle: Text(
+                  DateFormat.yMMMd().format(_transactions[index].date)
+                ),
+              ),
+            );
+          },
+          itemCount: _transactions.length,
+        ),
     );
   }
 }
