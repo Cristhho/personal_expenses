8bc4e0ba8851b21d05c8bedbe2a37c180faba26e
cristhian ochoa <cristhho@gmail.com>
2019-10-28 23:45:02 -0500

    checking if the device is in landscape mode

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,10 +11,10 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: _transactions.isEmpty ?
-        Column(
+    return _transactions.isEmpty ?
+    LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
           children: <Widget>[
             Text(
               "No transactions added yet",
@@ -24,43 +24,44 @@ class TransactionList extends StatelessWidget {
               height: 10,
             ),
             Container(
-              height: 200,
-              child: Image.asset("assets/img/waiting.png", fit: BoxFit.cover,)
+                height: constraints.maxHeight * 0.6,
+                child: Image.asset("assets/img/waiting.png", fit: BoxFit.cover,)
             )
           ],
-        )
-        : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-              elevation: 5,
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 30,
-                  child: Padding(
-                    padding: EdgeInsets.all(6.0),
-                    child: FittedBox(
-                      child: Text('\$${_transactions[index].amount}')
-                    ),
-                  ),
-                ),
-                title: Text(
-                  _transactions[index].title,
-                  style: Theme.of(context).textTheme.title,
-                ),
-                subtitle: Text(
-                  DateFormat.yMMMd().format(_transactions[index].date)
-                ),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  color: Theme.of(context).errorColor,
-                  onPressed: () {deleteTransaction(_transactions[index].id);},
+        );
+      }
+    )
+    : ListView.builder(
+      itemBuilder: (ctx, index) {
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+          elevation: 5,
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: Padding(
+                padding: EdgeInsets.all(6.0),
+                child: FittedBox(
+                  child: Text('\$${_transactions[index].amount}')
                 ),
               ),
-            );
-          },
-          itemCount: _transactions.length,
-        ),
+            ),
+            title: Text(
+              _transactions[index].title,
+              style: Theme.of(context).textTheme.title,
+            ),
+            subtitle: Text(
+              DateFormat.yMMMd().format(_transactions[index].date)
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              color: Theme.of(context).errorColor,
+              onPressed: () {deleteTransaction(_transactions[index].id);},
+            ),
+          ),
+        );
+      },
+      itemCount: _transactions.length,
     );
   }
 }
